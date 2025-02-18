809888dad442cf46b87b680f861b7edac652c284
Elena <elena.gelzinyte@gmail.com>
2025-02-18 12:29:03 +0100

    implement naive per-element loss weights

--- mace/tools/efg_utils.py
+++ mace/tools/efg_utils.py
@@ -1,3 +1,4 @@
+import numpy as np
 import logging
 import ast
 
@@ -7,14 +8,16 @@ from mace import data, modules, tools
 def get_efgs_per_element_weights(loss_element_weights, train_loader, z_table):
         
     if loss_element_weights is None:
-         efgs_element_weights = 1/modules.scaling_classes["efgs_mean_eval_scaling"](data_loader=train_loader)
+        efgs_element_weights = 1/modules.scaling_classes["efgs_mean_eval_scaling"](data_loader=train_loader)
         logging.info(f"No efg per-element loss weights were given, computing from data")
     else:
         logging.info(f"Using efg per-element loss weights from command line") 
         try:
-            efgs_element_weights = ast.literal_eval(loss_element_weights)
+            efgs_element_weights_dict = ast.literal_eval(loss_element_weights)
         except Exception as e:
-            raise RuntimeError(f"los element weights specified invalidly, error {e} occured") from e
+            raise RuntimeError(f"Loss element weights specified invalidly, error {e} occured") from e
+
+        efgs_element_weights = np.array([efgs_element_weights_dict[z] for z in z_table.zs])
 
     logging.info(f'using weighted efg loss with weights: {efgs_element_weights}')
 
